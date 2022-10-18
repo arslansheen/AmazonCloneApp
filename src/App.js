01950c005d8d0082e6e7dd885d810c12b2ef4da2
arslan sheen <arslan.sheen@gmail.com>
2022-10-18 16:27:28 +0500

    added react router dom and created checkout page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Header from './Components/Header.jsx'
 import Body from './Components/Body.jsx'
+import CheckOut from './CheckOut';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 function App() {
   return (
-    <>
+    // <>
+<Router>
 <Header/>
-<Body/>
-    </>
+< Routes>
+<Route path='/checkout' element={<CheckOut/>}/>
+<Route path='/' element={<>
+ <Body/>
+</>}>
+
+</Route>
+</Routes> 
+</Router>
+    // </>
 
     );
 }
---
+++ src/CheckOut.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './CheckOut.css';
+export default function CheckOut(props) {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <div className="checkout__ad">
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668.jpg"
+            alt=""
+          />
+        </div>
+        <div className="checkout__basket"></div>
+      </div>
+      <div className="checkout__right"></div>
+    </div>
+  );
+}
