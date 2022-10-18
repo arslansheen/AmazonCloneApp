d7127f0ab8ef8ea692eb06bd4894a16c348b37d8
arslan sheen <arslan.sheen@gmail.com>
2022-10-18 21:52:26 +0500

    added react-router-dom Link  to the amazon logo

--- src/CheckOut.jsx
+++ src/CheckOut.jsx
@@ -9,6 +9,9 @@ export default function CheckOut(props) {
             src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668.jpg"
             alt=""
           />
+          <div className="checkout__title">
+            <h2>Your Shopping Basket</h2>
+          </div>
         </div>
         <div className="checkout__basket"></div>
       </div>
--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Logo.css';
 export default function Logo() {
   return (
-    <img
-      className="header__logo"
-      src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-      alt=""
-    />
+    <Link to="/" className="header__logoLink">
+      <img
+        className="header__logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
+    </Link>
   );
 }
