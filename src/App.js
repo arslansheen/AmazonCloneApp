b480a6e3450fe8c2ee5a71df6641e73d19743de6
arslan sheen <arslan.sheen@gmail.com>
2022-10-27 17:14:13 +0500

    added payment page and imported stripe api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import './App.css';
-import React from 'react';
-import Header from './Components/Header.jsx'
-import Body from './Components/Body.jsx'
-import CheckOut from './Components/CheckOut';
-import Payment from './Components/Payment';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
 import Login from './Components/Login';
+import './App.css';
+import React from 'react';
+import Header from './Components/Header.jsx'
+import Body from './Components/Body.jsx'
+import CheckOut from './Components/CheckOut';
+import Payment from './Components/Payment';
+import { loadStripe } from '@stripe/stripe-js';
+import {Elements} from '@stripe/react-stripe-js';
+//load stripe into a promise
+const stripePromise=loadStripe("pk_test_51LxPodCI8b6kimS58gpyXgnPIWzKW498y8AVJo8klNH4PdPdBkdscydEfF8LlLgCiVFSjFDQH1fkVumPnYo0uVbC0032D8TwXk")
+
+
 function App() {
   return (
     // <>
@@ -19,7 +25,9 @@ function App() {
 <Route path='/payment' element={
 <>
 <Header/>
+<Elements stripe={stripePromise}>
 <Payment/>
+</Elements>
 </>
 }/>
   <Route path='/checkout' element={
--- src/Components/Payment.jsx
+++ src/Components/Payment.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useStateValue } from '../StateProvider';
 import BasketItem from './BasketItem';
+import CurrencyFormat from 'react-currency-format';
 import './Payment.css';
+import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
 const Payment = () => {
+  const [disabled, setDisabled] = useState(true);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [status, setStatus] = useState({});
   const [{ user, basket }, dispatch] = useStateValue();
+  const totalPrice = basket.reduce(
+    (container, currentItem) => container + currentItem.price,
+    0
+  );
   return (
     <div className="payment">
       <div className="payment__container">
-        <h1>Check Out ({basket?.length})</h1>
+        <h1>
+          Check Out
+          <Link to="/checkout"> ({basket?.length} items)</Link>
+        </h1>
         <div className="payment__section">
           <div className="payment__title">
             <h3>Delivery Address</h3>
           </div>
-          <p>{user?.email || ''}</p>
-          <p>123 React Lane</p>
-          <p>Log Angeles,CA</p>
+          <div className="payment__address">
+            <p>{user?.email || ''}</p>
+            <p>123 React Lane</p>
+            <p>Log Angeles,CA</p>
+          </div>
         </div>
-        <div className="payment__section">
-          <div className="payment__title">
+        <div className="payment__section ">
+          <div>
             <h3>Review Items and delivery</h3>
-            <div className="payment__items">
+            <div className="payment__customSection">
               {basket.map((item) => {
                 return (
                   <BasketItem
@@ -34,12 +49,49 @@ const Payment = () => {
           </div>
         </div>
         <div className="payment__section">
-          <div className="payment__title"></div>
-          <h3>Payment Method</h3>
-          <div className="payment__details">{/* Stripe  */}</div>
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            <form onSubmit={handleOnSumit}>
+              <CardElement onChange={handleOnChange} />
+            </form>
+            <div className="payment__priceContainer">
+              <CurrencyFormat
+                value={totalPrice}
+                renderText={(value) => (
+                  <>
+                    <h3>Order Total: {value}</h3>
+                  </>
+                )}
+                decimalScale={2}
+                displayType={'text'}
+                thousandSeparator={true}
+                prefix="$"
+              />
+              <button disabled={status !== 'ok'}>
+                <span>{status === 'submit' ? 'Processing' : 'Buy Now'}</span>
+              </button>
+            </div>
+            {status?.error ? status.error : ''}
+          </div>
         </div>
       </div>
     </div>
   );
+  function handleOnChange(e) {
+    if (e.empty) {
+      setStatus(e.empty ? 'empty' : 'ok');
+    } else if (e?.error) {
+      setStatus(e?.error ? { error: e.error.message } : 'ok');
+    } else {
+      setStatus('ok');
+    }
+  }
+  async function handleOnSumit(e) {
+    e.preventDefault();
+    console.log('buy button clicked');
+    setStatus('submit');
+  }
 };
 export default Payment;
